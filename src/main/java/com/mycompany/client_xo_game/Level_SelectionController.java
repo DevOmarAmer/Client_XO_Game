cbcfcb980332217457c458ed8f3563c05fa60734
Omar Amer <omahar1907@gmail.com>
2025-12-28 22:37:39 +0200

    build Level Selection screen UI

---
+++ src/main/java/com/mycompany/client_xo_game/Level_SelectionController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.client_xo_game;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author Omar
+ */
+public class Level_SelectionController implements Initializable {
+
+    @FXML
+    private Button easy_level_id;
+    @FXML
+    private Button medium_level_id;
+    @FXML
+    private Button hard_level_id;
+    @FXML
+    private Button back_id;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
