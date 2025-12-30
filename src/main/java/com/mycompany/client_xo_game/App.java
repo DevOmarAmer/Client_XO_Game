8bcd7173f54c8e2edb9bd33f4fe86d3ae39f655c
Omar Amer <omahar1907@gmail.com>
2025-12-30 14:03:31 +0200

    Merge branch 'main' of https://github.com/DevOmarAmer/Client_XO_Game

--- src/main/java/com/mycompany/client_xo_game/App.java
+++ src/main/java/com/mycompany/client_xo_game/App.java
@@ -8,9 +8,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
     private static Scene scene;
@@ -19,20 +16,21 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("Win_Lose"), 640, 480);
         stage.setScene(scene);
+        stage.setTitle("XO Game");
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
+    public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader
+                = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
         launch();
     }
-
 }
